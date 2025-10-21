d2b14088a6b2418f57a8bba95ad1d93b9af078eb
lily <ilovesuno@proton.me>
2025-10-21 16:44:16 -0700

    Load all configuration files

--- Neovim/init.lua
+++ Neovim/init.lua
@@ -1,1 +1,20 @@
-require("dashboard") -- Dashboard manager ~ Alpha.nvim
+local path = vim.fn.stdpath("config") .. "/lua"
+
+local function require_files(directory, prefix)
+    prefix = prefix or ""
+
+    for _, file in ipairs(vim.fn.readdir(directory)) do
+        local full_path = directory .. "/" .. file
+        local stat = vim.loop.fs_stat(full_path)
+
+        if stat then
+            if stat.type == "file" and file:match("%.lua$") and file ~= "pacman.lua" then
+                require(prefix .. file:gsub("%.lua$", ""))
+            elseif stat.type == "directory" then
+                require_files(full_path, prefix .. file .. ".")
+            end
+        end
+    end
+end
+
+require_files(path)
