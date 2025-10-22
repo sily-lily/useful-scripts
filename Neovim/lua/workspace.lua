18890f43feaa5d82a7b78d58fa2ea5d9ca5ae4ad
lily <ilovesuno@proton.me>
2025-10-21 18:25:10 -0700

    chore: add keybind and auto startup

--- Neovim/lua/workspace.lua
+++ Neovim/lua/workspace.lua
@@ -7,6 +7,7 @@
 ]]
 
 local pacman = require("pacman")
+local KeybindManager = require("binder")
 
 pacman.use("https://github.com/nvim-tree/nvim-tree.lua", function()
     require("nvim-tree").setup({
@@ -27,4 +28,18 @@ pacman.use("https://github.com/nvim-tree/nvim-tree.lua", function()
             dotfiles = true
         }
     })
+
+    local function open_on_startup()
+        local api = require("nvim-tree.api")
+        local stats = vim.loop.fs_stat(vim.fn.getcwd())
+
+        if stats and stats.type == "directory" then
+            api.tree.open()
+        end
+    end
+
+    vim.schedule(open_on_startup)
+    KeybindManager.map("n", "Alt + G", function()
+        require("nvim-tree.api").tree.toggle()
+    end)
 end)
