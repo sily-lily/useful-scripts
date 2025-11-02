1f96c1589e40da63569f7f964dec21c5852fe599
lily <ilovesuno@proton.me>
2025-11-01 19:01:39 -0700

    Change header and keybinds

--- Neovim/lua/dashboard.lua
+++ Neovim/lua/dashboard.lua
@@ -1,39 +1,26 @@
---[[
-
-    ~ Dashboard configured using Alpha.nvim ~
-
-              October 11th, 2025
-
-]]
-
-local pacman = require("pacman")
+local pacman = require("toolkits.pacman")
 
 pacman.use("https://github.com/goolord/alpha-nvim", function()
     local dashboard = require("alpha.themes.dashboard")
 
     dashboard.section.header.val = {
-        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠛⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠛⢠⡀⠸⣆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⡁⢰⣋⡇⠀⡿⢳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠁⣏⠉⠛⠳⢤⣟⠀⢧⠀⠀⣀⣤⣠⣤⣄⣀⠀⠀⢷⠀⠀⠈⠙⠢⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡔⠃⣀⠘⢆⣀⠀⠀⠉⠀⠘⠚⠉⠀⠀⢀⡀⠀⢸⠇⣀⢸⡀⠀⠀⠀⠀⠈⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⠙⡾⠟⣷⠂⠉⠀⠀⠀⠀⠀⠀⠀⢶⢚⣹⠃⢠⡏⠀⡏⠹⠃⠀⠀⠀⠀⠀⢸⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣼⠇⣰⠀⠀⠀⠠⠶⠳⣦⠀⠀⠀⠘⠲⠃⢠⠟⠁⠀⡏⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠈⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠏⡟⠷⠟⠁⠀⠀⣴⡆⠀⢸⡇⠀⠀⢠⣄⡴⠏⠀⡀⢰⠇⠀⠀⠀⠀⠀⠀⢠⡾⠚⠋⠉⠉⢳⡀⠀⠀⠀⠀⠀",
-        "⠀⢹⡌⠙⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⢧⡐⠶⠄⠀⠀⠻⣃⡀⠸⡷⠀⠀⠀⠹⣆⠀⢀⡿⡏⠀⠀⠀⠀⠀⠀⣠⡾⠀⠀⠀⢀⡴⠟⠁⠀⠀⠀⠀⠀",
-        "⠀⠀⢷⠀⠀⠻⣷⣄⠀⠀⠀⠀⠀⠀⢀⣀⡀⠀⠙⠦⣤⣀⡀⠀⠘⠿⠇⠀⣤⠴⣶⣞⣁⣠⣾⡀⠀⠀⣤⣠⣴⠶⢾⣁⣧⠀⠀⠀⠈⢧⡤⠖⠚⠦⣄⡀⠀",
-        "⠀⠀⠘⡇⠀⠀⠈⠻⣷⡀⠀⠀⣠⢾⡉⢉⡍⠙⠳⣶⢟⣯⣭⠿⠷⣤⡀⣠⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠛⠚⠻⠀⠀⠀⠀⠈⣇⠀⠀⠀⠀⠙⣦",
-        "⠀⠀⠀⢹⡀⠀⠀⠀⢿⣧⠀⠀⢧⣸⡀⠘⣇⣴⠀⠀⢘⠛⠛⠀⣰⠊⠻⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣤⣄⣄⡓⣶⠏",
-        "⠀⠀⠀⠀⣧⠀⠀⠀⠈⣿⣆⣠⣤⣭⡭⠿⣹⣿⡋⠉⠛⠓⠒⠴⠃⠀⠀⠀⠀⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣇⠈⠙⠛⠁⠀",
-        "⠀⠀⠀⠀⠈⠳⣄⠀⠀⠸⣿⠉⠀⠈⢻⠞⠙⡾⠁⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣀⣀⣀⣀⣀⣀⣀⣬⠷⠶⠤⢤⣤⣄⣀⣀⣤⣄⡀⣰⠤⢽⠆⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⠀⠈⠳⣄⠀⢿⣧⠀⠀⠘⠷⠾⠷⣼⣅⣀⣀⠀⠀⠀⠀⠀⠀⠸⡅⠀⠀⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠏⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⠀⠀⠀⠘⢧⡈⡿⣄⣀⣀⣀⣀⣀⣈⣳⣍⠉⠉⠛⢿⡛⢦⡼⠛⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⠻⠶⠿⠿⠷⠷⠿⠿⠾⠶⠶⠶⠶⠿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+        "  ░██ ░██░██                      ░██                 ",
+        "  ░██    ░██                                          ",
+        "  ░██ ░██░██ ░██    ░██░██    ░██ ░██░█████████████   ",
+        "  ░██ ░██░██ ░██    ░██░██    ░██ ░██░██   ░██   ░██  ",
+        "  ░██ ░██░██ ░██    ░██ ░██  ░██  ░██░██   ░██   ░██  ",
+        "  ░██ ░██░██ ░██   ░███  ░██░██   ░██░██   ░██   ░██  ",
+        "  ░██ ░██░██  ░█████░██   ░███    ░██░██   ░██   ░██  ",
+        "                    ░██                               ",
+        "               ░███████                               ",
     }
 
     dashboard.section.buttons.val = {
-        dashboard.button("N", "  New file", ":ene <CR>"), -- Shift N
-        dashboard.button("Q", "  Quit", ":qa<CR>"),       -- Shift Q
+        dashboard.button("N", "  Make New", ":ene <CR>"),         -- Shift   N
+        dashboard.button("Alt + F", "  Find File", nil),          -- Alt     F
+        dashboard.button("Alt + T", "󰙅  File Tree Preview", nil),  -- Alt     G
+        dashboard.button("Control + P", "  View Plugins", nil),   -- Control P
+        dashboard.button("Q", "  Quit", ":qa<CR>"),               -- Shift   Q
     }
 
     local loaded, failed, skipped = pacman.stats()
@@ -41,24 +28,27 @@ pacman.use("https://github.com/goolord/alpha-nvim", function()
         string.format("Packages loaded: %d ⬩ Failed: %d ⬩ Skipped: %d", loaded, failed, skipped)
     }
 
-    do
-        local padding = math.floor((vim.fn.winheight(0) - (#dashboard.section.header.val + #dashboard.section.buttons.val + #dashboard.section.footer.val + 8)) / 2)
-
-        dashboard.config.layout = {
-            {
-                type = "padding",
-                val = padding
-            }, dashboard.section.header,
-            {
-                type = "padding",
-                val = 2
-            }, dashboard.section.buttons,
-            {
-                type = "padding",
-                val = 2
-            }, dashboard.section.footer
-        }
+    local height = vim.fn.winheight(0)
+    if height == 0 then
+        height = 24
     end
+    
+    local padding = math.floor((height - (#dashboard.section.header.val + #dashboard.section.buttons.val + #dashboard.section.footer.val + 8)) / 2)
+
+    dashboard.config.layout = {
+        {
+            type = "padding",
+            val = padding
+        }, dashboard.section.header,
+        {
+            type = "padding",
+            val = 2
+        }, dashboard.section.buttons,
+        {
+            type = "padding",
+            val = 2
+        }, dashboard.section.footer
+    }
 
     require("alpha").setup(dashboard.opts)
 end)
